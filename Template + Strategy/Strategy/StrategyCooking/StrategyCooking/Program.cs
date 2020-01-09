0837fb05d4c073ec5bbfd4ad65c1c847ef13b61c
Christopher Bernold <christopherbernold@icloud.com>
2020-01-09 12:23:55 +0100

    removed a bool that wasnt used in strategy

--- Template + Strategy/Strategy/StrategyCooking/StrategyCooking/Program.cs
+++ Template + Strategy/Strategy/StrategyCooking/StrategyCooking/Program.cs
@@ -16,27 +16,22 @@ namespace StrategyCooking
             Console.WriteLine($"So you want to cook some {food} eh? You can grill, slow cook or bake your food.");
             Console.WriteLine("Type your cooking method:");
             string input = Console.ReadLine();
-            Boolean goodInput = true;
                 switch (input)
                 {
                     case "grill":
                         cookMethod.SetCookStrat(new Grilling());
                         cookMethod.Cook();
-                        goodInput = false;
                         break;
                     case "bake":
                         cookMethod.SetCookStrat(new Baking());
                         cookMethod.Cook();
-                        goodInput = false;
                         break;
                     case "slow cook":
                         cookMethod.SetCookStrat(new SlowCooker());
                         cookMethod.Cook();
-                        goodInput = false;
                         break;
                     default:
                         Console.WriteLine("I dont think I know how to cook like that try again.");
-                        goodInput = true;
                     break;
                 }
             Console.ReadKey();
